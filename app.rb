1ff4017fab84ba9549998d461426c9e65d5277cc
Nadia <nodunayo@gmail.com>
2013-07-27 10:15:56 +0100

    Added content

--- app.rb
+++ app.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
 
+set :port, 8000
+set :bind, '127.0.0.1'
+
 get '/' do
+    @message = "Hello!"
     erb :index
 end
+
+
+get '/shoe/:name' do
+    name = params[:name].capitalize
+    @message ="Hello #{name}, nice shoes!"
+    @image_path = 'shoe.jpg'
+     erb :shoe
+end 
+
+get '/shoe/:name/lots' do
+    name = params[:name].capitalize
+    @message ="Hello #{name}, nice shoes! I like them a lot."
+     erb :index
+end 
+
+get '/shoe/:name/:from' do
+    name = params[:name].capitalize
+    from = params[:from].capitalize
+   @message = "Hello #{name}, nice shoes! -From #{from}."
+     erb :index
+end 
+
