a13f601f655334a946356231e2ec2b87b2f8427a
Nadia <nodunayo@gmail.com>
2013-07-29 00:20:48 +0100

    Added content

--- app.rb
+++ app.rb
@@ -4,28 +4,49 @@ set :port, 8000
 set :bind, '127.0.0.1'
 
 get '/' do
-    @message = "Hello!"
+    @message = "Do your chores!!"
     erb :index
 end
 
 
-get '/shoe/:name' do
-    name = params[:name].capitalize
-    @message ="Hello #{name}, nice shoes!"
-    @image_path = 'shoe.jpg'
-     erb :shoe
-end 
-
-get '/shoe/:name/lots' do
-    name = params[:name].capitalize
-    @message ="Hello #{name}, nice shoes! I like them a lot."
-     erb :index
-end 
-
-get '/shoe/:name/:from' do
+get '/clean/:name/:from' do
     name = params[:name].capitalize
     from = params[:from].capitalize
-   @message = "Hello #{name}, nice shoes! -From #{from}."
-     erb :index
+    @message ="Clean the house, #{name}! Thanks, #{from}."
+    @image_path = 'shoe.jpg'
+    erb :index
 end 
+
+get '/bedroom/:name/:from' do
+    name = params[:name].capitalize
+    from = params[:from].capitalize
+    @message ="Your bedroom could do with a tidy, #{name}! Thanks, #{from}."
+    @image_path = 'shoe.jpg'
+    erb :index
+end 
+
+get '/vacuum/:name/:from' do
+    name = params[:name].capitalize
+    from = params[:from].capitalize
+    @message ="Isn't it your turn to do the vacuuming, #{name}? Thanks, #{from}."
+    @image_path = 'shoe.jpg'
+    erb :index
+end 
+
+get '/rubbish/:name/:from' do
+    name = params[:name].capitalize
+    from = params[:from].capitalize
+    @message ="Please take out the bins, #{name}! Thanks, #{from}."
+    @image_path = 'shoe.jpg'
+    erb :index
+end 
+
+get '/kitches/:name/:from' do
+    name = params[:name].capitalize
+    from = params[:from].capitalize
+    @message ="The dishes have been piling up in the sink, #{name}! Thanks, #{from}."
+    @image_path = 'shoe.jpg'
+    erb :index
+end 
+
 
