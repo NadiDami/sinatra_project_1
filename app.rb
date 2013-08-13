43a57cb4f48dd22deae3e41a34ea883209ff4dce
Nadia <nodunayo@gmail.com>
2013-08-13 01:09:15 +0100

    Heroku env variables

--- app.rb
+++ app.rb
@@ -16,8 +16,8 @@ Pony.options = {
       :address              => 'smtp.gmail.com',
       :port                 => '587',
       :enable_starttls_auto => true,
-      :user_name            => ENV['USER_NAME'],
-      :password             => ENV['PASSWORD'],
+      :user_name            => ENV['GMAIL_USER_NAME'],
+      :password             => ENV['GMAIL_PASSWORD'],
       :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
       :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
     }
