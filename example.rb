1fcfb8f78ad19c1ca9b2f5a77929740ebc446896
Nadia Odunayo <NadiDami@Nadia-Odunayos-MacBook-Pro.local>
2013-10-11 10:28:52 +0100

    Removed example.rb

--- example.rb
+++
@@ -1,5 +0,0 @@
-Testing to see what happens  two people try  push some changes to the same file to the develop branch, without having giiiiiit before pushing.
-
-"I'm sorry, Hamilton. I failed at the testing exercise! Please forgive me."
-
-I solved the conflict
